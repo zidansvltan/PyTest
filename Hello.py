9c48517bbfa496d27a0449a3d954fbbc1a8b5d34
Dorovski Montgomery <71650014+zidansvltan@users.noreply.github.com>
2022-05-11 20:02:30 +0700

    New input and command

--- Hello.py
+++ Hello.py
@@ -1,6 +1,7 @@
 x = int(input())
+y = int(input())
 
-if x == 1:
-    print('HelloWorld!')
-else:
-    print('WorldHello!')
+if x > y:
+    print('Hello World!')
+elif x < y:
+    print('hElLO WOrLd!')
